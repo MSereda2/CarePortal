b29ed3932516d45566ec3772f75f648785ead8fc
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2020-01-28 23:44:16 -0600

    Modified actions and state

--- src/components/map/Map.component.jsx
+++ src/components/map/Map.component.jsx
@@ -1,47 +1,33 @@
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import actions from '../../redux/actions/actionsMap';
+import React from "react";
+import { connect } from "react-redux";
+import GoogleMapReact from "google-map-react";
+import { getCoordinates } from "../../redux/map/map.actions";
 
+const MyMap = props => {
+  console.log("Nice ", props.state);
+  return (
+    <GoogleMapReact
+      //   bootstrapURLKeys={"AIzaSyB8W9IB-mRvjpjMa5AxJ0Ea3Hy8biThLvI"}
+      defaultCenter={props.defaultCenter}
+      defaultZoom={props.defaultZoom}
+    >
+      {
+        <div>
+          <button onClick={() => props.getLocation()}>click me</button>
+          <h1>{props.value}</h1>
+        </div>
+      }
+    </GoogleMapReact>
+  );
+};
 
+const mapStateToProps = state => ({
+  state,
+  defaultCenter: state.map.defaultCenter,
+  defaultZoom: state.map.defaultZoom
+});
+const mapDispatchToProps = dispatch => ({
+  getLocation: () => dispatch(getCoordinates())
+});
 
-const MyMap = (props) => {
-    const OnGetLocation = () => {
-        props.dispatch(actions.getCoordinateAction());
-    }
-   
-    let MyMap = () => (
-        <GoogleMapReact
-            bootstrapURLKeys={props.defult.bootstrapURLKeys}
-            defaultCenter={props.defult.defaultCenter}
-            defaultZoom={props.defult.defaultZoom}>
-                {<div>
-                <button onClick={OnGetLocation}>click me</button>
-                <p>{console.log(props.defult.latitude)}</p>
-                <p>{props.defult.longitude}</p>
-            </div>
-            }
-        </GoogleMapReact>
-    )
-    
-    return(
-      <MyMap />  
-    )
-}
-
-
-
-export default MyMap;
-
-
-
-// const getLocation = () => {
-//     if(navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(getCoordinates)
-//     } else {
-//         alert(`doesn't work`)
-//     }
-// }
-
-// const getCoordinates = (position) => {
-//     console.log(position);
-// }
+export default connect(mapStateToProps, mapDispatchToProps)(MyMap);
