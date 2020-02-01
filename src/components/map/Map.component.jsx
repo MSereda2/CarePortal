d56ba54a0f958ecf91dc82b6c0ad11f9bf7c5cc9
Misha Sereda <sereda2510@mail.ru>
2020-02-01 06:36:48 +0300

    hooks is ready

--- src/components/map/Map.component.jsx
+++ src/components/map/Map.component.jsx
@@ -8,12 +8,30 @@ import { getCoordinates } from "../../redux/map/map.actions";
 
 
 const WrappedMap = props => {
+
+  const [latitude, setLatitude] = useState(props.latitude);
+  const [longitude, setLongitude] = useState(props.longitude);
+
+  let getLocation = () => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getCordinates)
+    } else {
+      alert('doesnt worl')
+    }
+  }
+
+   let getCordinates = (position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude)
+  }
+
   const myMap = () => (
-      <GoogleMap
+    <GoogleMap
       defaultZoom={props.defaultZoom} 
       defaultCenter={{lat: props.defaultCenter.lat, lng: props.defaultCenter.lng}} >
             <Marker key={25} position={{lat: props.latitude, lng: props.longitude}} />
-            <button onClick={props.getLocation}>click</button>
+            <button onClick={getLocation}>click</button>
+            <p>{`${latitude}/${longitude}`}</p>
     </GoogleMap>
   )
   const WrappedMap = withScriptjs(withGoogleMap(myMap))
@@ -24,9 +42,7 @@ const WrappedMap = props => {
       googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
       loadingElement={<div style={{height: '100%'}}></div>}
       containerElement={<div style={{height: '100%'}}></div>}
-      mapElement={<div style={{height: '100%'}}></div>}>
-       
-    </WrappedMap>
+      mapElement={<div style={{height: '100%'}}></div>} />
   )
 }
 
