1b1ab5facf488b8f1c73ab3b5119dc6c971c501d
Misha Sereda <sereda2510@mail.ru>
2020-02-13 21:02:18 +0300

    convert map into class component

--- src/components/pages/taskPage/mapContainer/Map.jsx
+++ src/components/pages/taskPage/mapContainer/Map.jsx
@@ -3,227 +3,70 @@ import { withGoogleMap, GoogleMap, withScriptjs, InfoWindow, Marker, InfoBox } f
 import Autocomplete from 'react-google-autocomplete';
 import Geocode from "react-geocode";
 import PopUp from "reactjs-popup";
-import { Button } from '@material-ui/core';
-import TaskOnMap from './taskOnMap/TaskOnMap'
 
 
+class Map extends React.Component {
 
-let Map = (props) => {
-
-    const [selectedTask, setTask] = useState(null);
-    const [coordinates, setCoord] = useState({lat: props.userLocation.latitude, lng: props.userLocation.longitude});
+        constructor( props ){
+            super( props );
+            this.state = {
+                latitude: this.props.userLocation.latitude,
+                longitude: this.props.userLocation.longitude,
+                selectedTask: null
+            };
+        }
+      
+        userLocation = () => {
+            if(this.state.latitude !== 55.755825) {
+                return <Marker icon position={{lat: this.state.latitude, lng: this.state.longitude}} />
+            } else {
+                console.log(this.state.latitude)
+            }
+        }
     
-        let userLocation = () => {
-        if(coordinates.lat !== 55.755825) {
-            return <Marker position={{lat: coordinates.lat, lng: coordinates.lng}} />  
-        } else {
-           console.log(coordinates.latitude)
-        }}
-
-        let getCoordinates = (position) => {
-            setCoord({
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-        })}
-
-        useEffect(() => {
-
+        componentDidMount = () => {
             if(navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(getCoordinates)
+                navigator.geolocation.getCurrentPosition(this.getCoordinates)
             } else {
                 alert('woooo')
             }
-        }, [])
-
-
-        return()
-}
-
-
-export default Map ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let Map = (props) => {
-
-//     const [selectedTask, setTask] = useState(null);
-//     const [coordinates, setCoord] = useState({lat: props.userLocation.latitude, lng: props.userLocation.longitude});
-
-//     let userLocation = () => {
-//         if(coordinates.lat !== 55.755825) {
-//             return <Marker position={{lat: coordinates.lat, lng: coordinates.lng}} />  
-//         } else {
-//            console.log(coordinates.latitude)
-//         }
-//     }
-
-//     let getCoordinates = (position) => {
-//         setCoord({
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude
-//     })}
-
-//     useEffect(() => {
-
-//         if(navigator.geolocation) {
-//                 navigator.geolocation.getCurrentPosition(getCoordinates)
-//         } else {
-//             alert('woooo')
-//         }
-//     }, [])
-
-
-//     const AsyncMap = withScriptjs(
-//          withGoogleMap(() => ( 
-//            <GoogleMap
-//               defaultZoom={16}
-//               defaultCenter={{ lat: coordinates.lat, lng: coordinates.lng }}>
-
-//                 {userLocation()}
-
-//                 {props.taskOnMap.map(task => (<Marker id={task.id} onClick={() => setTask(task)}  position={{lat: task.coordinates.lat, lng: task.coordinates.lng}}></Marker>))}        
-
-//                 {selectedTask && (
-//                     <InfoWindow position={{lat: selectedTask.coordinates.lat,lng: selectedTask.coordinates.lng}}>
-//                         <PopUp trigger={ } modal>
-//                             <span> modal</span>
-//                         </PopUp>
-//                     </InfoWindow>
-//                 )}
-//            </GoogleMap>)),
-//     );
+        }
     
-
-//     let map = <AsyncMap googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDGe5vjL8wBmilLzoJ0jNIwe9SAuH2xS_0&libraries=places&key=${process.env.REACT_APP_GOOGLE_KEY}`} loadingElement={ <div style={{ height: `100%` }} />} containerElement={ <div style={{ height: '89vh' }} />} mapElement={<div style={{ height: `100%` }} />}/>
+        getCoordinates = (position) => {
+            this.setState({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            })
+        }
     
-//     return(map)
-// }
-
-
-// export default Map;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Map extends React.Component{
-
-//         constructor( props ){
-//             super( props );
-//             this.state = {
-//                 latitude: this.props.userLocation.latitude,
-//                 longitude: this.props.userLocation.longitude,
-//                 selectedTask: null
-//             };
-//         }
-      
-//         userLocation = () => {
-//             if(this.state.latitude !== 55.755825) {
-//                 return <Marker icon position={{lat: this.state.latitude, lng: this.state.longitude}} />
-//             } else {
-//                 console.log(this.state.latitude)
-//             }
-//         }
+        render(){
     
-//         componentDidMount = () => {
-//             if(navigator.geolocation) {
-//                 navigator.geolocation.getCurrentPosition(this.getCoordinates)
-//             } else {
-//                 alert('woooo')
-//             }
-//         }
+            const AsyncMap = withScriptjs(
+               withGoogleMap(
+                props => ( 
+                 <GoogleMap
+                 defaultZoom={16}
+                 defaultCenter={{ lat: this.state.latitude, lng: this.state.longitude }}>
+                     {this.userLocation()}
     
-//         getCoordinates = (position) => {
-//             this.setState({
-//                 latitude: position.coords.latitude,
-//                 longitude: position.coords.longitude
-//             })
-//         }
-    
-//          render(){
-    
-//             const AsyncMap = withScriptjs(
-//                withGoogleMap(
-//                 props => ( 
-//                  <GoogleMap
-//                  defaultZoom={16}
-//                  defaultCenter={{ lat: this.state.latitude, lng: this.state.longitude }}>
-//                      {this.userLocation()}
-    
-//                      {props.taskOnMap.map(task => (<Marker id={task.id} onClick={() => setTask(task)}  position={{lat: task.coordinates.lat, lng: task.coordinates.lng}}></Marker>))}        
-
-//                  {selectedTask && (
-//                      <InfoWindow position={{lat: selectedTask.coordinates.lat,lng: selectedTask.coordinates.lng}}>
-//                         <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est ratione fugit harum sed facere sunt, eum dolores ea quas. Odio, laboriosam! Repellat unde accusantium eos quasi harum tempore natus doloribus?</div>
-//                      </InfoWindow>
-//                  )}
+                     {this.props.taskOnMap.map(task => (<Marker id={task.id} position={{lat: task.coordinates.lat, lng: task.coordinates.lng}}></Marker>))}        
     
                     
-//                  </GoogleMap>)),
-//                 );
+                 </GoogleMap>)),
+                );
     
-//             let map =   
-//                 <AsyncMap
-//                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGe5vjL8wBmilLzoJ0jNIwe9SAuH2xS_0&libraries=places"
-//                 loadingElement={
-//                 <div style={{ height: `100%` }} />
-//                 }
-//                 containerElement={
-//                 <div style={{ height: '89vh' }} />
-//                 }
-//                 mapElement={
-//                 <div style={{ height: `100%` }} />
-//                 }/>
+            let map = <AsyncMap
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGe5vjL8wBmilLzoJ0jNIwe9SAuH2xS_0&libraries=places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: '89vh' }} />}
+                mapElement={<div style={{ height: `100%` }} />
+            }/>
          
     
     
     
-//               return( map )
-//              }
-//             }        
+              return( map )
+        }
+}        
             
-//     export default Map
+    export default Map
