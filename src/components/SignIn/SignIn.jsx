098b022e10420a230db8da82f43e27dda4632a2c
Misha Sereda <sereda2510@mail.ru>
2020-03-16 07:32:36 +0300

    small changes

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import style from './signIn.module.css';
 
+import singIn from '../../assets/signin-image.jpg'
+
 import {signInWithGoogle} from '../../firebase/firebase.utils.js';
 import FormInput from '../common/formInput/FormInput';
+import FormCheckBox from '../common/formCheckBox/FormCheckBox';
 
 const SingIn = (props) => (
     <div className={style.singIn}>
         <div className={style.left_column}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ex, corporis saepe explicabo quia dolore nesciunt sint veniam! Possimus accusantium quasi minima laboriosam explicabo eum. Ullam qui adipisci voluptatibus nostrum!
+            <img src={singIn} alt=""/>
         </div>
         <div className={style.right_column}>
             <h2 className={style.singup_heading}>Войти</h2>
             <form action="">
                 <FormInput />
                 <FormInput />
+                <FormCheckBox />
                 
                 <button>Войти</button>
             </form>
